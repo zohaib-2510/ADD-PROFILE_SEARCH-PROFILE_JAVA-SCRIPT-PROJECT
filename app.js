814707b4de280c229f0eaaf3a2ebc8d0a17593ca
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-10-04 06:37:31 -0700

    g

---
+++ app.js
@@ -0,0 +1,8 @@
+const serpro = document.querySelector(".serpro");
+const addpro = document.querySelector(".addpro");
+const richpro = document.querySelector(".richpro")
+const arrpro = document.querySelector(".arrpro")
+
+serpro.addEventListener("click",function(){
+    window.location.replace('https://www.');
+})
