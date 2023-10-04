c5617e06272ab8ff31eec8c9e5e8b7550c1af62d
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-10-04 12:02:47 -0700

    bb

--- app.js
+++ app.js
@@ -6,36 +6,11 @@ function myFunction(){
         var full = document.querySelector(".profile")
         var name1 = document.getElementById("name1")
         var con = document.getElementById("conn")
-        var full2 = document.querySelector(".profile2").style.display="none"
         var full1 = document.querySelector("form")
         btn.addEventListener("click",function(){
             name1.innerHTML=inp1;
             con.innerHTML=inp2;
             full.style.display="flex"
-            console.log(inp1);
-            
-            full1.style.display="none"
-            btn.innerHTML="again click"
-            full1.style.display="block"
-        })
-        
-    }
-
-    myFunction()
-function myFunction(){
-
-    var btn = document.getElementById("btn")
-    var inp1 = document.getElementById("inp1").value;
-        var inp2 = document.getElementById("inp2").value;
-        var full = document.querySelector(".profile")
-        var full2 = document.querySelector(".profile2")
-        var name1 = document.getElementById("name1")
-        var con = document.getElementById("conn")
-        var full1 = document.querySelector("form")
-        btn.addEventListener("click",function(){
-            name1.innerHTML=inp1;
-            con.innerHTML=inp2;
-            full2.style.display="flex"
             console.log(inp1);
             
             full1.style.display="none"
