5a25a635644cb91c5ee05aef8a6a7e557a0a1a77
zohaib-2510 <140811807+zohaib-2510@users.noreply.github.com>
2023-10-04 11:48:16 -0700

    mj

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const serpro = document.querySelector(".serpro");
-const addpro = document.querySelector(".addpro");
-const richpro = document.querySelector(".richpro")
-const arrpro = document.querySelector(".arrpro")
+function myFunction(){
 
-serpro.addEventListener("click",function(){
-    window.location.replace('https://www.');
-})
+    var btn = document.getElementById("btn")
+    var inp1 = document.getElementById("inp1").value;
+        var inp2 = document.getElementById("inp2").value;
+        var full = document.querySelector(".profile")
+        var name1 = document.getElementById("name1")
+        var con = document.getElementById("conn")
+        var full1 = document.querySelector("form")
+        btn.addEventListener("click",function(){
+            name1.innerHTML=inp1;
+            con.innerHTML=inp2;
+            full.style.display="flex"
+            console.log(inp1);
+            
+            full1.style.display="none"
+            btn.innerHTML="again click"
+            full1.style.display="block"
+            var p2 = document.querySelector(".profile2").style.display="flex"
+        })
+        
+    }
+    myFunction()
